6ef3c0fbf7297a9a539e357b559c09be9325d333
koss <jobs.koss@gmail.com>
2025-06-27 19:26:51 +0900

    build: Интеграция PrimeVue с настройками темы и добавление Pinia для управления состоянием

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { createApp } from 'vue'
 import './assets/styles/index.scss'
+import 'primeicons/primeicons.css'
 import router from './router'
+import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 
-const app = createApp(App)
+const MyPreset = definePreset(Aura, {})
 
-app.use(router)
+const app = createApp(App)
+const pinia = createPinia()
+
+app.use(router).use(pinia)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: MyPreset,
+    options: {
+      cssLayer: {
+        name: 'primevue',
+        order: 'app-styles, primevue, another-css-library',
+      },
+    },
+  },
+})
 
 app.mount('#app')
