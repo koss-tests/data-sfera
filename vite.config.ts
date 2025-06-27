3568ba4566f87a4249ed17914a5550119e040432
koss <jobs.koss@gmail.com>
2025-06-27 19:25:53 +0900

    build: Обновлена конфигурация Vite для поддержки PrimeVue, добавлены новые алиасы

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,31 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
   server: {
-    port: 3000
+    port: 3000,
   },
   preview: {
-    port: 3000
+    port: 3000,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@types': path.resolve(__dirname, './src/shared/types')
-    }
-  }
+      '@store': path.resolve(__dirname, './src/store'),
+      '@types': path.resolve(__dirname, './src/shared/types'),
+    },
+  },
 })
