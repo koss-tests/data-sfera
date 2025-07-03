9c3f582df43a2a0100281bf75e5d4b14a22227f4
koss <jobs.koss@gmail.com>
2025-07-03 15:43:27 +0900

    feat: Добавлен компонент Loader для отображения состояния загрузки

---
+++ src/components/Loader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { isLoading } = defineProps<{
+  isLoading: boolean
+}>()
+</script>
+
+<template>
+  <div>
+    <slot v-if="isLoading" name="loading">Загрузка...</slot>
+    <slot v-else></slot>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
