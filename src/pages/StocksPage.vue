a3a785b93c7fb6d625f2fcf2157f6ebf69fe2675
koss <jobs.koss@gmail.com>
2025-07-03 15:49:03 +0900

    feat: Добавлены графики и индикаторы загрузки на страницу складов, улучшен интерфейс с использованием компонентов

--- src/pages/StocksPage.vue
+++ src/pages/StocksPage.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Loader } from '@/components'
+import { WarehousesStockBarChart } from '@/components/charts'
+import useStocksStore from '@store/stocksStore'
+
+const stocksStore = useStocksStore()
+
+onMounted(async () => {
+  await stocksStore.getStocksData()
+})
+</script>
 
 <template>
-  <div>Stocks Page</div>
+  <h1>Склады</h1>
+  <div>
+    <Loader :is-loading="stocksStore.isStocksDataLoading">
+      <WarehousesStockBarChart :data="stocksStore.stocksData" />
+    </Loader>
+    <br />
+    <p>
+      Пример таблиц в <RouterLink :to="{ name: 'incomes' }">Доходы</RouterLink> и
+      <RouterLink :to="{ name: 'orders' }">Заказы</RouterLink>
+    </p>
+  </div>
 </template>
