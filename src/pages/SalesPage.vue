b99ffddc91b5ca1f3db9f9a4d23f525c23ca1cf5
koss <jobs.koss@gmail.com>
2025-07-03 15:48:50 +0900

    feat: Добавлены графики и индикаторы загрузки на страницу продаж, улучшен интерфейс с использованием компонентов

--- src/pages/SalesPage.vue
+++ src/pages/SalesPage.vue
@@ -1,5 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { Loader } from '@/components'
+import { PriceDiscountScatterChart, SalesTrendLineChart } from '@components/charts'
+import useSalesStore from '@/store/salesStore'
+
+const salesStore = useSalesStore()
+
+onMounted(async () => {
+  await salesStore.getSalesData()
+})
+</script>
 
 <template>
-  <div>Sales Page</div>
+  <h1>Продажи</h1>
+  <div>
+    <Loader :is-loading="salesStore.isSalesDataLoading">
+      <SalesTrendLineChart :data="salesStore.salesData" />
+    </Loader>
+    <br />
+    <Loader :is-loading="salesStore.isSalesDataLoading">
+      <PriceDiscountScatterChart :data="salesStore.salesData" />
+    </Loader>
+  </div>
+  <br />
+  <p>
+    Пример таблиц в <RouterLink :to="{ name: 'incomes' }">Доходы</RouterLink> и
+    <RouterLink :to="{ name: 'orders' }">Заказы</RouterLink>
+  </p>
 </template>
