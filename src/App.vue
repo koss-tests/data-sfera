b9c5c6c0a27fac888cba030109d7dd8917539e95
koss <jobs.koss@gmail.com>
2025-07-03 15:43:44 +0900

    feat: Добавлен кнопка для управления боковой панелью и обновлен стиль кнопки

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <script setup lang="ts">
+import useAppStore from '@store/index'
+import Sidebar from '@components/Sidebar.vue'
+
+const appStore = useAppStore()
+
+const pt = {
+  root: {
+    class: 'menu-btn',
+  },
+}
 </script>
 
 <template>
+  <Button
+    icon="pi pi-bars"
+    size="small"
+    severity="secondary"
+    rounded
+    aria-label="Star"
+    @click="appStore.toggleSidebar"
+    :pt
+  />
+  <Sidebar />
   <router-view />
 </template>
+
+<style lang="scss" scoped>
+.menu-btn {
+  position: fixed;
+  top: 1rem;
+  left: 1rem;
+}
+</style>
