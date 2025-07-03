5a00309bc4066016503b690673cbdd8573003e9a
koss <jobs.koss@gmail.com>
2025-07-03 15:42:58 +0900

    build: Обновлен импорт стилей и добавлены семантические настройки для темы Aura

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './assets/styles/index.scss'
+import '@shared/assets/styles/index.scss'
 import 'primeicons/primeicons.css'
 import router from './router'
 import { createPinia } from 'pinia'
@@ -8,7 +8,23 @@ import { definePreset } from '@primeuix/themes'
 import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 
-const MyPreset = definePreset(Aura, {})
+const MyPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{sky.50}',
+      100: '{sky.100}',
+      200: '{sky.200}',
+      300: '{sky.300}',
+      400: '{sky.400}',
+      500: '{sky.500}',
+      600: '{sky.600}',
+      700: '{sky.700}',
+      800: '{sky.800}',
+      900: '{sky.900}',
+      950: '{sky.950}',
+    },
+  },
+})
 
 const app = createApp(App)
 const pinia = createPinia()
